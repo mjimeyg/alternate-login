fc5b5dcd929437a867c27e32770f8cf3b05d5f6a
Martin James <martin.jim@hotmail.co.uk>
2012-11-22 22:09:55 +0000

    Hook file, first commit.

---
+++ root/includes/hooks/hooks_alternatelogin.php
@@ -0,0 +1,60 @@
+<?php
+
+class CSAlternateLogin
+{
+	function page_header(&$hook)
+	{
+		global $template, $user, $phpbb_root_path, $config;
+		
+		$result = $hook->previous_hook_result('phpbb_user_session_handler');
+		
+		// Begin Alternate Login code
+        require_once($phpbb_root_path . 'includes/functions_alternatelogin.php'); // Include the functions for Alternate Login module
+        $user->add_lang('mods/info_ucp_alternatelogin');
+        if($user->data['session_fb_access_token'])
+        {
+            $graph_url = "https://graph.facebook.com/me?" . $user->data['session_fb_access_token'];
+
+
+            $fb_user = json_decode(get_fb_data($graph_url));
+            
+            $fb_lang = $fb_user->locale;
+        }
+		
+		$template->assign_vars(array(
+			'S_AL_FB_ENABLED'				=> isset($config['al_fb_login']) ? $config['al_fb_login'] : false,
+			'S_AL_TW_ENABLED'				=> isset($config['al_tw_login']) ? $config['al_tw_login'] : false,
+			'S_AL_WL_ENABLED'				=> isset($config['al_wl_login']) ? $config['al_wl_login'] : false,
+            'S_AL_OI_ENABLED'                               => isset($config['al_oi_login']) ? $config['al_oi_login'] : false,
+			'S_AL_WL_USER'					=> isset($user->data['al_wl_id']) ? $user->data['al_wl_id'] : false,
+			'S_AL_FB_USER'                                  => isset($user->data['al_fb_id']) ? $user->data['al_fb_id'] : false,
+			'S_AL_TW_USER'                                  => isset($user->data['al_tw_id']) ? $user->data['al_tw_id'] : false,
+			'S_AL_OI_USER'                                  => isset($user->data['al_oi_id']) ? $user->data['al_oi_id'] : false,
+			'AL_FB_APPID'					=> isset($config['al_fb_id']) ? $config['al_fb_id'] : false,
+			'AL_FB_SITE_DOMAIN'                             => isset($config['al_site_domain']) ? $config['al_site_domain'] : false,
+			'AL_FB_ACTIVITY'                                => isset($config['al_fb_activity']) ? $config['al_fb_activity'] : false,
+			'AL_FB_FACEPILE'                                => isset($config['al_fb_facepile']) ? $config['al_fb_facepile'] : false,
+			'AL_FB_STREAM'                                  => isset($config['al_fb_stream']) ? $config['al_fb_stream'] : false,
+			'AL_FB_LIKE_BOX'                                => isset($config['al_fb_like_box']) ? $config['al_fb_like_box'] : false,
+			'S_AL_WL_CLIENT_ID'				=> isset($config['al_wl_client_id']) ? $config['al_wl_client_id'] : false,
+			'S_AL_WL_WRAP_CALLBACK'                         => isset($config['al_wl_callback']) ? $config['al_wl_callback'] : false,
+			'S_AL_WL_WRAP_CHANNEL'                          => isset($config['al_wl_channel']) ? $config['al_wl_channel'] : false,
+			'AL_FB_APP_ID'                                  => isset($config['al_fb_id']) ? $config['al_fb_id'] : false,
+			'AL_FB_PAGE_URL'                                => isset($config['al_fb_page_url']) ? $config['al_fb_page_url'] : false,
+			'FB_APP_ID'                                     => isset($config['al_fb_id']) ? $config['al_fb_id'] : false,
+			'AL_FB_USER_HIDE_ACTIVITY'                      => isset($user->data['al_fb_hide_activity']) ? $user->data['al_fb_hide_activity'] : false,
+			'AL_FB_USER_HIDE_STREAM'                        => isset($user->data['al_fb_hide_stream']) ? $user->data['al_fb_hide_stream'] : false,
+			'AL_FB_USER_HIDE_FACEPILE'                      => isset($user->data['al_fb_hide_facepile']) ? $user->data['al_fb_hide_facepile'] : false,
+			'AL_FB_USER_HIDE_LIKE_BOX'                      => isset($user->data['al_fb_hide_like_box']) ? $user->data['al_fb_hide_like_box'] : false,
+			'U_AL_WL_AUTHORIZE'                             => "https://oauth.live.com/authorize?client_id={$config['al_wl_client_id']}&scope=wl.signin%20wl.basic%20wl.birthday%20wl.emails%20wl.work_profile%20wl.postal_addresses&response_type=code&redirect_uri=" . urlencode($config['al_wl_callback']),
+			'U_AL_TW_REQUEST'                               => "{$phpbb_root_path}alternatelogin/al_tw_connect.{$phpEx}?authentication=1",
+			'U_AL_OI_LOGIN'                                 => "{$phpbb_root_path}alternatelogin/al_oi_auth.{$phpEx}",
+			'S_FB_LOCALE'                                   => ($fb_lang) ? $fb_lang : 'en_GB',
+			'S_RETURN_TO_PAGE'                              => "?return_to_page=" . base64_encode(build_url()),
+		));
+	}
+}
+
+$phpbb_hook->register('phpbb_user_session_handler', array('CSAlternateLogin', 'page_header'));
+
+?>
