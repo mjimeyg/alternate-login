a9628cad39cf8c4da467cb66cfd35ea51ff51b08
Martin James <martin.jim@hotmail.co.uk>
2013-02-26 15:08:11 +0000

    Fixed various errors and added account linking code.

--- root/styles/prosilver/template/alternatelogin.js
+++ root/styles/prosilver/template/alternatelogin.js
@@ -62,29 +62,28 @@ function is_user_registered(al_user_id, login_type){
 
 function is_email_valid(e_mail)
 {
+	var dataObj = {
+		'email' : e_mail
+	};
+	var ret_val = true;
 	$.ajax({
 		url:'./alternatelogin/ajax/is_email_valid.php',
-		processData: false,
-		data: {email : e_mail},
+		data: dataObj,
 		dataType: 'json',
 		async : false,
 		success: function(data){
-			console.log(data.status);
-			if(data.status == 0){
-				console.log('Invalid email: ' + data.message)
-				show_dialog('Email Issue', data.message);
-				return false;
-			}else{
-				console.log('Good Email');
-				return true;
-			}
+			console.log(data);
+			
+			ret_val = data;
+			
 			
 		},
 		error: function(data){
 			console.log('phpBB user login FAILURE.');
-			return false;
+			ret_val = false;
 		}
 	});
+	return ret_val;
 }
 
 function fb_register_user(){
@@ -221,10 +220,16 @@ function fb_user_login(fb_id, email){
 				console.log('FB user is not registered');
 				
 				// Check for existing useage of email and if appropriate ask if they want to register
-				if(is_email_valid(email) == false){
+				var valid_email = is_email_valid(email);
+				console.log('Valid Email: ' + valid_email.status);
+				if(valid_email.status == 0 && valid_email.message == 'EMAIL_TAKEN'){
 					// Offer account linking
-					console.log(ret);
-					console.log('Email is NOT valid');	
+					if(duplicate_emails){
+						show_dialog('Link accounts?', 'This account already exists.  Would you like to link it or create a new account?', [{'name':'Link Accounts'},{'name':'Create New Account'}]);
+					}
+					else{
+						show_dialog('Link accounts?', 'This account already exists.  Would you like to link it?', [{'name':'Link Accounts'},{'name':'Cancel'}], link_accounts);
+					}
 					return;
 				}
 				console.log('Email is valid');
@@ -242,15 +247,69 @@ function fb_user_login(fb_id, email){
 	});
 }
 
+function link_accounts(e){
+	console.log('link_accounts() called.');
+	
+	
+	
+	if($(this).attr('id') == 'Link Accounts'){
+		console.log('Link Accounts clicked.');
+		verify_password('link');
+	}
+	else if($(this).attr('id') == 'Create New account'){
+		console.log('Create new Account clicked.');
+	}
+	else{
+		console.log('Cancel clicked.');
+		close_dialog();
+	}
+}
+
+function verify_password(action){
+	console.log('verify_password()');
+	var password_form = $('<form id="verify_password_form" method="post" action="' + './alternatelogin/al_fb_connect.php?action=' + action + '">');
+	var fieldset = $('<fieldset>').appendTo(password_form);
+	var dl = $('<dl>').appendTo(fieldset);
+	var dd = $('<dd>').appendTo(dl);
+	var label = $('<label>')
+					.attr('for', 'password_field')
+					.html('Please enter your board password:')
+					.appendTo(dd);
+	var dt = $('<dt>').appendTo(dl);
+	var input = $('<input type="password">')
+					.attr({
+						
+						'id' : 'password_field',
+						'name' : 'password_field'
+					})
+					.appendTo(dt);
+	
+	show_dialog('Please enter your board password.', password_form, [{'name':'Submit'},{'name': 'Cancel'}], submit_password);
+}
+
+function submit_password(e){
+	console.log('password_form() called.');
+	
+	if($(this).attr('id') == 'Submit'){
+		console.log('Submit clicked.');
+		$('#verify_password_form').submit();
+	}
+	else{
+		console.log('Cancel clicked.');
+		close_dialog();
+	}
+}
+
 /**
 *
 *	Params:
 *
-*	buttons : 'ok', 'ok-cancel'
+*	buttons : json array [{'name':'Ok'},{'name':'Cancel'}]
 *
 **/
 function show_dialog(title, message, buttons, callback){
 	console.log(message);
+	$('.modal_dialog_user_buttons').unbind('click');
 	var mask = $('#mask');
 	
 	var dialog = $('#modal_dialog');
@@ -262,11 +321,17 @@ function show_dialog(title, message, buttons, callback){
 	
 	var dialog_buttons = $('#modal_dialog_buttons');
 	
+	var dialog_close_button = $("#modal_dialog_close");
+	
+	dialog_title.html('');
+	dialog_message.html('');
+	dialog_buttons.html('');
+	
 	mask.css({
 			'width' : $(window).width(),
 			'height' : $(document).height()
 		})
-		.fadeIn(1000)
+		.fadeIn(300)
 		.fadeTo('slow', 0.8);
 	
 	dialog_title.html(title);
@@ -284,36 +349,50 @@ function show_dialog(title, message, buttons, callback){
 	//dialog_panel.width(dialog.width() - 10);
 	
 	// Setup the dialog buttons
-	switch(buttons){
-		case 'ok/cancel':
-			break;
-			
-		case 'ok':
-		default:
-			
-		 break;
-	}
-	var ok_button = $('<input type="button" id="dialog_ok">')
-								.attr({
-									'value' : 'Ok'
-								})
-								
-								.appendTo(dialog_buttons);
 	
-	ok_button.click(function(){
-		if(typeof callback == "function"){
-			callback();
-		}
-		close_dialog();
+	$.each(buttons, function(key, value){
+		$('<button type="button" id="' + value.name + '">')
+									.html(value.name)
+									.addClass('modal_dialog_user_buttons')
+									.appendTo(dialog_buttons);
 	});
+	
+	console.log(typeof callback);
+	if(typeof callback == "function"){
+		console.log('callback called');
+		$('.modal_dialog_user_buttons').bind('click', callback);
+	}else{
+		$('.modal_dialog_user_buttons').bind('click', close_dialog);
+		console.log('default called');
+	}
+
+	
+	dialog_close_button.click(function(e) {
+        e.preventDefault();
+		
+		close_dialog();
+    });
 	
 	dialog.fadeIn(2000);
 }
 
 function close_dialog(){
+	$('.modal_dialog_user_buttons').unbind('click');
+	
 	var mask = $('#mask');
 	
 	var dialog = $('#modal_dialog');
+	
+	var dialog_title = $('#modal_dialog_title');
+	var dialog_message = $('#modal_dialog_message');
+	
+	var dialog_buttons = $('#modal_dialog_buttons');
+	
+	
+	
+	dialog_title.html('');
+	dialog_message.html('');
+	dialog_buttons.html('');
 	
 	dialog.hide('fast');
 	
