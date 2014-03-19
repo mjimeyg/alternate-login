1050ec5ef43fb107f23956e1bbaae2986983a1bf
Martin James <martin.jim@hotmail.co.uk>
2014-03-19 21:52:49 +0000

    Altered files to work with new library.

--- root/alternatelogin/al_oi_auth.php
+++ root/alternatelogin/al_oi_auth.php
@@ -22,12 +22,13 @@ define('IN_PHPBB', true);
 $phpbb_root_path = (defined('PHPBB_ROOT_PATH')) ? PHPBB_ROOT_PATH : '../';
 $phpEx = substr(strrchr(__FILE__, '.'), 1);
 
+
 // Load include files.
 include($phpbb_root_path . 'common.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_user.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_profile_fields.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_alternatelogin.' . $phpEx);// Add the custom phpBB Alternate Login functions file
-include($phpbb_root_path . 'alternatelogin/openid/openid.' . $phpEx);
+include($phpbb_root_path . 'alternatelogin/openid/common.' . $phpEx);// Add the custom phpBB Alternate Login functions file
 
 if($config['al_oi_login'] == 0)
 {
@@ -43,57 +44,75 @@ $user->setup('ucp');
 $user->add_lang('mods/info_acp_alternatelogin');
 $user->add_lang('mods/info_ucp_alternatelogin');// Global Alternate Login language file.
 
-$openid_identifier = request_var('openid_identifier', '');
+$openid = request_var('openid_identifier', '');
 
 try
 {
-    $openid = new LightOpenID($config['server_name']);
-  
-    if(!$openid->mode)
-    {
-        if($openid_identifier != '')
-        {
-             
-            $openid->identity = $openid_identifier;
-            
-            $openid->required = array('contact/email');
+    $consumer = getConsumer();
 
-            header('Location: ' . $openid->authUrl());
-        }
+    // Begin the OpenID authentication process.
+    $auth_request = $consumer->begin($openid);
+
+    // No auth request means we can't begin OpenID.
+    if (!$auth_request) {
+        trigger_error("Authentication error; not a valid OpenID.");
     }
-    elseif($openid->mode == 'cancel')
-    {
-        trigger_error('You have canceled your login.');
+
+    $sreg_request = Auth_OpenID_SRegRequest::build(
+                                     // Required
+                                     array('nickname'),
+                                     // Optional
+                                     array('fullname', 'email'));
+
+    if ($sreg_request) {
+        $auth_request->addExtension($sreg_request);
     }
-    else
-    {
-        if($openid->validate())
-        {
-            $openid_identifier = request_var('openid_claimed_id', '');
 
-            $attributes = $openid->getAttributes();
-            $url = "{$phpbb_root_path}alternatelogin/al_oi_connect.$phpEx?openid_identifier=" . urlencode($openid_identifier);
-            if(isset($attributes['contact/email']))
-            {
-                $url .= "&openid_email=" . urlencode($attributes['contact/email']);
-            }
+	$policy_uris = null;
+	if (isset($_GET['policies'])) {
+    	$policy_uris = $_GET['policies'];
+	}
 
-            redirect(append_sid($url));
+    $pape_request = new Auth_OpenID_PAPE_Request($policy_uris);
+    if ($pape_request) {
+        $auth_request->addExtension($pape_request);
+    }
 
+    // Redirect the user to the OpenID server for authentication.
+    // Store the token for this authentication so we can verify the
+    // response.
+
+    // For OpenID 1, send a redirect.  For OpenID 2, use a Javascript
+    // form to send a POST request to the server.
+    if ($auth_request->shouldSendRedirect()) {
+        $redirect_url = $auth_request->redirectURL(getTrustRoot(),
+                                                   getReturnTo());
+
+        // If the redirect URL can't be built, display an error
+        // message.
+        if (Auth_OpenID::isFailure($redirect_url)) {
+            trigger_error("Could not redirect to server: " . $redirect_url->message);
+        } else {
+            // Send redirect.
+            header("Location: ".$redirect_url);
         }
-        else
-        {
-            
-            trigger_error($user->lang['FAILED_TO_AUTHENTICATE']);
+    } else {
+        // Generate form markup and render it.
+        $form_id = 'openid_message';
+        $form_html = $auth_request->htmlMarkup(getTrustRoot(), getReturnTo(),
+                                               false, array('id' => $form_id));
+
+        // Display an error if the form markup couldn't be generated;
+        // otherwise, render the HTML.
+        if (Auth_OpenID::isFailure($form_html)) {
+            trigger_error("Could not redirect to server: " . $form_html->message);
+        } else {
+            print $form_html;
         }
     }
 }
-catch(ErrorException $ex)
+catch(Exception $ex)
 {
     trigger_error($ex->getMessage());
 }
-
-
-
-
 ?>
--- root/alternatelogin/al_oi_connect.php
+++ root/alternatelogin/al_oi_connect.php
@@ -27,7 +27,7 @@ include($phpbb_root_path . 'common.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_user.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_profile_fields.' . $phpEx);
 include($phpbb_root_path . 'includes/functions_alternatelogin.' . $phpEx);// Add the custom phpBB Alternate Login functions file
-include($phpbb_root_path . 'alternatelogin/openid/openid.' . $phpEx);
+include($phpbb_root_path . 'alternatelogin/openid/common.' . $phpEx);
 
 if($config['al_oi_login'] == 0)
 {
@@ -43,8 +43,88 @@ $user->setup('ucp');
 $user->add_lang('mods/info_acp_alternatelogin');
 $user->add_lang('mods/info_ucp_alternatelogin');// Global Alternate Login language file.
 
-$openid_identifier = request_var('openid_identifier', '');
-$openid_email = request_var('openid_email', '');
+$consumer = getConsumer();
+
+// Complete the authentication process using the server's
+// response.
+$return_to = getReturnTo();
+$response = $consumer->complete($return_to);
+
+// Check the response status.
+if ($response->status == Auth_OpenID_CANCEL) 
+{
+	// This means the authentication was cancelled.
+	trigger_error('Verification cancelled.');
+} 
+else if ($response->status == Auth_OpenID_FAILURE) 
+{
+	// Authentication failed; display the error message.
+	trigger_error("OpenID authentication failed: " . $response->message);
+} 
+else if ($response->status == Auth_OpenID_SUCCESS) 
+{
+	// This means the authentication succeeded; extract the
+	// identity URL and Simple Registration data (if it was
+	// returned).
+	$openid = $response->getDisplayIdentifier();
+	$esc_identity = escape($openid);
+
+	
+
+	if ($response->endpoint->canonicalID) {
+		$escaped_canonicalID = escape($response->endpoint->canonicalID);
+		$success .= '  (XRI CanonicalID: '.$escaped_canonicalID.') ';
+	}
+
+	$sreg_resp = Auth_OpenID_SRegResponse::fromSuccessResponse($response);
+
+	$sreg = $sreg_resp->contents();
+
+	if (@$sreg['email']) {
+		$url_params .= "&openid_email=".escape($sreg['email']);
+	}
+
+	if (@$sreg['nickname']) {
+		$url_params .= "&openid_nickname=".escape($sreg['nickname']);
+	}
+
+	if (@$sreg['fullname']) {
+		$url_params .= "&openid_fullname=".escape($sreg['fullname']);
+	}
+
+	$pape_resp = Auth_OpenID_PAPE_Response::fromSuccessResponse($response);
+
+	/*if ($pape_resp) 
+	{
+		if ($pape_resp->auth_policies) {
+			$success .= "<p>The following PAPE policies affected the authentication:</p><ul>";
+	
+			foreach ($pape_resp->auth_policies as $uri) {
+				$escaped_uri = escape($uri);
+				$success .= "<li><tt>$escaped_uri</tt></li>";
+			}
+	
+			$success .= "</ul>";
+		} else {
+			$success .= "<p>No PAPE policies affected the authentication.</p>";
+		}
+	
+		if ($pape_resp->auth_age) {
+			$age = escape($pape_resp->auth_age);
+			$success .= "<p>The authentication age returned by the " .
+				"server is: <tt>".$age."</tt></p>";
+		}
+	
+		if ($pape_resp->nist_auth_level) {
+			$auth_level = escape($pape_resp->nist_auth_level);
+			$success .= "<p>The NIST auth level returned by the " .
+				"server is: <tt>".$auth_level."</tt></p>";
+		}
+
+	} else {
+            $success .= "<p>No PAPE response was sent by the provider.</p>";
+	}*/
+}
 
 $admin = request_var('admin', 0);
 // Select the user_id from the Alternate Login user data table which has the same Facebook Id.
@@ -126,10 +206,10 @@ else
 	if($user->data['user_id'] == ANONYMOUS)
 	{
             
-            $url = "{$phpbb_root_path}ucp.$phpEx?mode=register&openid_identifier=" . urlencode($openid_identifier);
+            $url = "{$phpbb_root_path}ucp.$phpEx?mode=register&openid_identifier=" . urlencode($esc_identity);
             if($openid_email != '')
             {
-                $url .= "&openid_email=" . urlencode($openid_email);
+                $url .= $url_params;
             }
 
             redirect(append_sid($url));
@@ -189,5 +269,10 @@ else
 	}
 
 }
+
+function escape($thing) {
+    return htmlentities($thing);
+}
+
 
 ?>
